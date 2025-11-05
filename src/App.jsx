9c4adec0a2a557e59ef1d0d576b2bc564487c641
Tiago Pitombeira <tiagopitombeira1984@gmail.com>
2025-11-05 09:31:54 -0300

    Configurando para Publicação no Git

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Form from './components/form'
+
+
+function App() {
+
+  return (
+    <div className="app">
+      <p>Meu Primeiro Formulário</p>
+      <Form/>
+    </div>
+  )
+}
+
+export default App
